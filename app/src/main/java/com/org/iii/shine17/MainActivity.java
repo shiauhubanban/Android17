c5e0336dd7aed41cde1ea6558da40b9b8f63f8b1
user <a>
2016-11-25 10:41:52 +0800

    v3 GPS

--- app/src/main/java/com/org/iii/shine17/MainActivity.java
+++ app/src/main/java/com/org/iii/shine17/MainActivity.java
@@ -1,25 +1,64 @@
 package com.org.iii.shine17;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private LocationManager lmgr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         webView = (WebView)findViewById(R.id.webView);
-        initWebView();
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    123);
+
+        }else {
+            init();
+        }
+
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+    public void init(){
+        initWebView();
+
+        lmgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //lmgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,);
+    }
+
     private void initWebView(){
         webView.setWebViewClient(new WebViewClient());
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/map.html");
+    }
+    public void gotoWhere(View v){
+        webView.loadUrl("javaScript:goto(36.043782, 32.721547)");
     }
 }
