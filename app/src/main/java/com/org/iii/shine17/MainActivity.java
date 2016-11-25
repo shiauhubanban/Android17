b7c9c39e6c41098222fc5e162e743c4b8766941e
user <a>
2016-11-25 09:58:20 +0800

    v2 GPS

--- app/src/main/java/com/org/iii/shine17/MainActivity.java
+++ app/src/main/java/com/org/iii/shine17/MainActivity.java
@@ -2,12 +2,24 @@ package com.org.iii.shine17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        initWebView();
+    }
+    private void initWebView(){
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/map.html");
     }
 }
